0937c08f1566e50f0934f7c835298ebf6c104280
Fernando Contreras <fernandomorann999@gmail.com>
2024-09-17 22:48:48 -0600

    primer commit

---
+++ Ejercicio1.ts
@@ -0,0 +1,8 @@
+// Estudiante: FERNANDO DAVID CONTRERAS MORAN
+let numero: number = 25;
+let texto: string = "Hola, TypeScript!";
+let booleano: boolean = true;
+
+console.log("Número:", numero);
+console.log("Texto:", texto);
+console.log("Booleano:", booleano);
